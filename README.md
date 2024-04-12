7ec6122bd7a8da5e24051baccf64579f4b2191f4
Alex Zaytsev <hazzik@gmail.com>
2024-04-12 10:47:06 +1000

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## A .NET Template project
---
+++ src/TemplateProjects.Tests/GreetingsTest.cs
@@ -0,0 +1,11 @@
+﻿namespace TemplateProjects.Tests;
+
+[TestFixture]
+public class GreetingsTest
+{
+    [Test]
+    public void Test()
+    {
+        Assert.That(Greetings.HelloWorld, Is.EqualTo("Hello world"));
+    }
+}
